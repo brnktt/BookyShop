c2ca7b76fd4836efc6c72ba627df4c89b69fc8f0
Alessandro Branchetti <brk.alessandro@gmail.com>
2022-01-10 20:40:35 +0100

    Project created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookyShopWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
