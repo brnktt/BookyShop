25d936c3886df6bdb6b6d3fd2bbd05ab8f9ff115
Alessandro Branchetti <brk.alessandro@gmail.com>
2022-01-13 22:46:01 +0100

    Created basic Category Model

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookyShopWeb.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+    }
+}
